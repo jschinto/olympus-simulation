0c18d9c56a8f1e176f2be153c0e78f8b6ce95c08
jam220 <jam220@lehigh.edu>
2019-02-28 16:54:42 -0500

    adding clients and procedure rooms

--- Simulation/app/src/main/java/com/olympus/simulation/SimulationActivity.java
+++ Simulation/app/src/main/java/com/olympus/simulation/SimulationActivity.java
@@ -1,7 +1,9 @@
 package com.olympus.simulation;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -12,7 +14,31 @@ public class SimulationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulation);
 
-        LinearLayout linearLayoutWaiting = findViewById(R.id.LinearLayoutWaiting);
-        LinearLayout linearLayoutProcedure = findViewById(R.id.LinearLayoutProcedure);
+    }
+
+    public void clickPlusIcon(View view) {
+        if (view.getId() == R.id.plusIconWaiting) {
+            LinearLayout linearLayoutClients = findViewById(R.id.LinearLayoutClients);
+            ImageView clientImage = new ImageView(getApplicationContext());
+            clientImage.setImageResource(R.drawable.client);
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(78, 100);
+            clientImage.setLayoutParams(layoutParams);
+
+            linearLayoutClients.addView(clientImage);
+            //TODO: add backend client here
+        }
+        else if (view.getId() == R.id.plusIconProcedure) {
+            LinearLayout linearLayoutRooms = findViewById(R.id.LinearLayoutRooms);
+            ImageView roomImage = new ImageView(getApplicationContext());
+            roomImage.setImageResource(R.drawable.procedure_room);
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(100, 100);
+            roomImage.setLayoutParams(layoutParams);
+            roomImage.setPadding(10,0,0,0);
+
+            linearLayoutRooms.addView(roomImage);
+            //TODO: add backend procedure room here
+        }
     }
 }
