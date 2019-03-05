3e0bc1b507542aa3dfff64d658fb01d345cc80b2
jam220 <jam220@lehigh.edu>
2019-03-05 17:47:32 -0500

    added more to info box

--- Simulation/app/src/main/java/com/olympus/simulation/SimulationActivity.java
+++ Simulation/app/src/main/java/com/olympus/simulation/SimulationActivity.java
@@ -3,6 +3,8 @@ package com.olympus.simulation;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -18,13 +20,15 @@ public class SimulationActivity extends AppCompatActivity {
 
     public void clickPlusIcon(View view) {
         if (view.getId() == R.id.plusIconWaiting) {
+
+
+
             LinearLayout linearLayoutClients = findViewById(R.id.LinearLayoutClients);
             ImageView clientImage = new ImageView(getApplicationContext());
             clientImage.setImageResource(R.drawable.client);
 
             LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(78, 100);
             clientImage.setLayoutParams(layoutParams);
-
             linearLayoutClients.addView(clientImage);
             //TODO: add backend client here
         }
@@ -40,5 +44,23 @@ public class SimulationActivity extends AppCompatActivity {
             linearLayoutRooms.addView(roomImage);
             //TODO: add backend procedure room here
         }
+    }
+
+
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.addClient) {
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
