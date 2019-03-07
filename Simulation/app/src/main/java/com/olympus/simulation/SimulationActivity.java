ff2e6d000b1d09277fe6cf10ca8f354f76f5da51
jam220 <jam220@lehigh.edu>
2019-03-07 15:12:30 -0500

    started integrating UI with backend

--- Simulation/app/src/main/java/com/olympus/simulation/SimulationActivity.java
+++ Simulation/app/src/main/java/com/olympus/simulation/SimulationActivity.java
@@ -6,23 +6,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SimulationActivity extends AppCompatActivity {
+
+    Simulation_Manager simulation_manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulation);
-
+        simulation_manager = new Simulation_Manager(0,100,1);
     }
 
     public void clickPlusIcon(View view) {
         if (view.getId() == R.id.plusIconWaiting) {
-
-
-
             LinearLayout linearLayoutClients = findViewById(R.id.LinearLayoutClients);
             ImageView clientImage = new ImageView(getApplicationContext());
             clientImage.setImageResource(R.drawable.client);
@@ -33,16 +35,79 @@ public class SimulationActivity extends AppCompatActivity {
             //TODO: add backend client here
         }
         else if (view.getId() == R.id.plusIconProcedure) {
-            LinearLayout linearLayoutRooms = findViewById(R.id.LinearLayoutRooms);
-            ImageView roomImage = new ImageView(getApplicationContext());
-            roomImage.setImageResource(R.drawable.procedure_room);
+            TextView headerView =  findViewById(R.id.textInfo_Name);
+            headerView.setText("Procedure Room");
 
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(100, 100);
-            roomImage.setLayoutParams(layoutParams);
-            roomImage.setPadding(10,0,0,0);
+            TextView textData1 = findViewById(R.id.textData1);
+            EditText editData1 = findViewById(R.id.editData1);
+            TextView textData2 = findViewById(R.id.textData2);
+            EditText editData2 = findViewById(R.id.editData2);
+            TextView textData3 = findViewById(R.id.textData3);
+            EditText editData3 = findViewById(R.id.editData3);
+            TextView textData4 = findViewById(R.id.textData4);
+            EditText editData4 = findViewById(R.id.editData4);
+            TextView textData5 = findViewById(R.id.textData5);
+            EditText editData5 = findViewById(R.id.editData5);
+            TextView textData6 = findViewById(R.id.textData6);
+            EditText editData6 = findViewById(R.id.editData6);
 
-            linearLayoutRooms.addView(roomImage);
-            //TODO: add backend procedure room here
+            textData1.setText("Travel Time:");
+            editData1.setText("");
+
+            textData2.setText("Cooldown Time:");
+            editData2.setText("");
+
+            textData3.setVisibility(View.INVISIBLE);
+            editData3.setVisibility(View.INVISIBLE);
+            textData4.setVisibility(View.INVISIBLE);
+            editData4.setVisibility(View.INVISIBLE);
+            textData5.setVisibility(View.INVISIBLE);
+            editData5.setVisibility(View.INVISIBLE);
+            textData6.setVisibility(View.INVISIBLE);
+            editData6.setVisibility(View.INVISIBLE);
+
+            findViewById(R.id.buttonEdit).setVisibility(View.INVISIBLE);
+
+        }
+        else if (view.getId() == R.id.buttonAddDelete) {
+            TextView headerView =  findViewById(R.id.textInfo_Name);
+            String header = headerView.getText().toString();
+            //press add button for procedure room
+            if (header.equals("Procedure Room")) {
+                int travelTime = 0;
+                int cooldownTime = 0;
+                try {
+                    EditText editData1 = findViewById(R.id.editData1);
+                    travelTime = Integer.parseInt(editData1.getText().toString());
+                    EditText editData2 = findViewById(R.id.editData2);
+                    cooldownTime = Integer.parseInt(editData2.getText().toString());
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getApplicationContext(), "Invalid Data Entered!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (cooldownTime <= 0 || travelTime <= 0) {
+                    Toast.makeText(getApplicationContext(), "Invalid Data Entered!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                ProcedureRoom procedureRoom = new ProcedureRoom(travelTime, cooldownTime);
+                simulation_manager.addProcedureRoom(procedureRoom);
+
+                LinearLayout linearLayoutRooms = findViewById(R.id.LinearLayoutRooms);
+                ImageView roomImage = new ImageView(getApplicationContext());
+                roomImage.setImageResource(R.drawable.procedure_room);
+
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(100, 100);
+                roomImage.setLayoutParams(layoutParams);
+                roomImage.setPadding(10, 0, 0, 0);
+
+                //get number or procedure rooms
+                roomImage.setTag(0,0);
+                roomImage.setTag(1,"Procedure Room");
+
+
+                linearLayoutRooms.addView(roomImage);
+            }
         }
     }
 
@@ -62,5 +127,14 @@ public class SimulationActivity extends AppCompatActivity {
         if (id == R.id.addClient) {
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick (View view) {
+        int index = (int)view.getTag(0);
+        String type = (String)view.getTag(1);
+
+        if (type.equals("Procedure Room")) {
+            //get prodecure room by index
+        }
     }
 }
